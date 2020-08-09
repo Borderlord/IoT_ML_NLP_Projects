5b891a2b9e5bab64f0ba20caea9e5980b184c266
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-08-09 06:20:49 +0000

    Project import generated by Copybara.

--- CSV File/csvdatalogging.py
+++ CSV File/csvdatalogging.py
@@ -7,4 +7,11 @@ try:
     for row in reader:
         print row
 finally:
-    f.close
+    f.close()
+"""
+g= open('/home/vikas/Downloads/vikas.csv','wt')
+writer=csv.writer(g)
+for i in range(0,10):
+ writer.writerow((i,i+10));
+g.close();
+
