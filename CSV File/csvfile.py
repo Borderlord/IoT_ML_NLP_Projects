15905b035e8885b95d097275c521e69516cef9b3
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-08-09 20:36:07 +0000

    ver bump

--- CSV File/csvfile.py
+++ CSV File/csvfile.py
@@ -1,1 +1,12 @@
-import c
+import csv
+
+"""g=open("/home/vikas/Desktop/file.csv",'wt');
+
+writer=csv.writer(g);
+
+for i in range(1,21):
+ writer.writerow((i,i*10));
+g.close();
+"""
+
+f=open('/home/vikas/Desktop/file.csv','rt');
