e141d3adf9c198fce256c0c2c5f20d052276bfb9
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-08-10 09:43:43 +0000

    merge and resolve conflicts

--- Get Post on Thingspeak/things.py
+++ Get Post on Thingspeak/things.py
@@ -2,4 +2,8 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 
-#data=urllib.urlopen("https:
+#data=urllib.urlopen("https://api.thingspeak.com/update?api_key=EDLC8KHZZ4EV4KKZ&field1="+str(900));
+#print data;
+
+
+datafromwebsite=urllib.urlopen("https://api.thingspeak.com/channels/289288
