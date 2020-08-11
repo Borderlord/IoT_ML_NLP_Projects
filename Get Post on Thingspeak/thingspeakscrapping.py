ade5e38c533a0c6c3bc4c8985ef40fcb8138e7a7
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-08-11 07:57:42 +0000

    Final Draft

--- Get Post on Thingspeak/thingspeakscrapping.py
+++ Get Post on Thingspeak/thingspeakscrapping.py
@@ -1,1 +1,8 @@
 import random
+import re
+import urllib
+from bs4 import BeautifulSoup
+
+b= random.randint(0,15000);
+# here i am posting data to the field1
+data=urllib.urlopen("https://api.thingspeak.
