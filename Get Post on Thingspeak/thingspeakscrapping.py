5f3870c4b795f1384692fc42c8b3978db1282e48
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-08-11 08:03:07 +0000

    localizations

--- Get Post on Thingspeak/thingspeakscrapping.py
+++ Get Post on Thingspeak/thingspeakscrapping.py
@@ -5,4 +5,8 @@ from bs4 import BeautifulSoup
 
 b= random.randint(0,15000);
 # here i am posting data to the field1
-data=urllib.urlopen("https://api.thingspeak.
+data=urllib.urlopen("https://api.thingspeak.com/update?api_key=CLRP7LZ00SCB6GKM&field1="+str(b));
+print data;
+
+
+fed=urllib.urlopen("https://api.thingspeak.com/channels/251862/fields/1.json?results=2")
