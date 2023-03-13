58cdf85845cdf1a9481fab833beccd4a8cc71cd3
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-03-13 08:17:23 +0000

    Removed folder from repository

--- Jarvis Personal Assistant/speechtotext.py
+++ Jarvis Personal Assistant/speechtotext.py
@@ -6,4 +6,9 @@ import webbrowser as wb
 import speech_recognition as sr
 r=sr.Recognizer()
 eng=pyttsx.init()
-locati
+location="INDIA" 
+
+eng.setProperty('rate',150);
+#eng.say("Hello Vikas ,i am Jarvis  What can i Do for you ? ");
+#eng.runAndWait()
+print("Hello Vikas ,i am Jar
