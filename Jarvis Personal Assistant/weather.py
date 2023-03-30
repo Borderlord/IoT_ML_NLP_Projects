db735282a583e71a7f7cf4c6e996fa728ae0d530
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-03-30 07:18:00 +0000

    EOD commit

--- Jarvis Personal Assistant/weather.py
+++ Jarvis Personal Assistant/weather.py
@@ -4,3 +4,13 @@ import webbrowser as wb
 
 inn = raw_input("Location :");
 
+
+def textfile():
+  with open("C:\Users\Vikas Yadav\Desktop\city_list.txt") as f:
+   for line in f:
+    if inn in line:
+       pb=re.findall('\d+', line) 
+       return pb; 
+b=textfile()
+
+html_s
