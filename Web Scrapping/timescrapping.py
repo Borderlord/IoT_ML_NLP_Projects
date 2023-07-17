6711281fcfed889534b2ed64ca9299395ae228c7
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-07-17 21:13:06 +0000

    updating repo

--- Web Scrapping/timescrapping.py
+++ Web Scrapping/timescrapping.py
@@ -2,4 +2,9 @@ import urllib
 from bs4 import BeautifulSoup
 
 html_doc = """
-<html><head><title>The Dormouse's story</t
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormtrse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href
