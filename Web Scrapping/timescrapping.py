413ea539c259c67fa9e2269d736b15e908db5e7b
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-07-27 19:36:32 +0000

    New translations en-US.json (Russian)

--- Web Scrapping/timescrapping.py
+++ Web Scrapping/timescrapping.py
@@ -7,4 +7,5 @@ html_doc = """
 <p class="title"><b>The Dormtrse's story</b></p>
 
 <p class="story">Once upon a time there were three little sisters; and their names were
-<a href
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
