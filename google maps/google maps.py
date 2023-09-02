fad04fb0d5d2af57b3dd2d545a5dc08e0ae6d91b
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-09-02 16:46:36 +0000

    Android 12 compatibility: add "android:exported" attribute to AndroidManifest.xml

--- google maps/google maps.py
+++ google maps/google maps.py
@@ -1,2 +1,6 @@
 import googlemaps
-from date
+from datetime import datetime
+gmaps = googlemaps.Client(key='AIzaSyBuUF2LTfdHTmbxmoVRRnYYFlyR-jCCgyQ')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('NSIT Dwarka Sect
