f160d161158640e28d4590b73ad2076566365b22
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-09-03 09:09:49 +0000

    [REF]

--- google maps/google maps.py
+++ google maps/google maps.py
@@ -3,4 +3,9 @@ from datetime import datetime
 gmaps = googlemaps.Client(key='AIzaSyBuUF2LTfdHTmbxmoVRRnYYFlyR-jCCgyQ')
 
 # Geocoding an address
-geocode_result = gmaps.geocode('NSIT Dwarka Sect
+geocode_result = gmaps.geocode('NSIT Dwarka Sector 3 , New Delhi')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via
