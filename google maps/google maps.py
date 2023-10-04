e725854a7dc4cbd28947a634f5b6f093877cfcad
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-10-04 19:07:55 +0000

    Test mqtt.publish

--- google maps/google maps.py
+++ google maps/google maps.py
@@ -8,4 +8,8 @@ geocode_result = gmaps.geocode('NSIT Dwarka Sector 3 , New Delhi')
 # Look up an address with reverse geocoding
 reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
-# Request directions via
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+           
