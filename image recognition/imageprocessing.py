2df309c52a25d49d2b2da884a0c9b1600efa84e9
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-10-14 07:30:31 +0000

    See last commit

--- image recognition/imageprocessing.py
+++ image recognition/imageprocessing.py
@@ -26,3 +26,59 @@ while(1):
     # Display the resulting frame
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+   
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('C://Users//Vikas Yadav//Desktop//output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+"""
+
+
+import numpy as np
+import cv2
+
+detector= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#cap = cv2.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
---
+++ play youtube video/playing youtube.py
@@ -0,0 +1,3 @@
+import webbrowser as wb
+"""
+wb.open_new_tab('C:\Users\Vikas Yadav\Desktop\shake.mp4'
