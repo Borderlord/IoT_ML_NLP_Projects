19a64f68297a67ad530de9f10e6fd312c284f434
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-10-17 09:14:22 +0000

    wrote a readme file

--- whatsapp browser automation/whatsapp.py
+++ whatsapp browser automation/whatsapp.py
@@ -1,1 +1,4 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys
