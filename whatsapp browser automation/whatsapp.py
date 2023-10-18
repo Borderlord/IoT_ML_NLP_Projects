b6a2183e0d9962d0667f985e67f2d9850dbaf228
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-10-18 06:14:02 +0000

    1.5.0

--- whatsapp browser automation/whatsapp.py
+++ whatsapp browser automation/whatsapp.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+ 
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = web
